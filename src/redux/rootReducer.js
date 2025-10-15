b217ce2aa5fb30ed21cf88b6dbde186a3db4c593
tanjilur.nabil <tanjilur.nabil@savethechildren.org>
2025-10-15 13:50:18 +0600

    store set up

---
+++ src/redux/rootReducer.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux'
+import filterReducer from './filters/reducer'
+import todoReducer from './todos/reducer'
+
+
+export const rootReducer = combineReducers({
+  todos: todoReducer,
+  filters: filterReducer
+})
